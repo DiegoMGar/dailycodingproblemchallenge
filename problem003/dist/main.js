d49659cc3507232811c646425d1eff97bc67511b
Diego Maroto <diego.diemg@gmail.com>
2018-11-28 21:03:49 +0100

    Serialize done, deserialize in progress

--- problem003/dist/main.js
+++ problem003/dist/main.js
@@ -3,28 +3,40 @@
  * @author Diego Maroto
  * @version 20181127
  */
-class CustomNode {
+class SolutionNode {
     constructor(val, left = undefined, right = undefined) {
         this.val = val;
         this.left = left;
         this.right = right;
     }
+    serialize(node) {
+        if (node === undefined) {
+            return '()';
+        }
+        return `(${node.val},${this.serialize(node.left)},${this.serialize(node.right)})`;
+    }
+    deserialize(serialized) {
+        console.log(serialized);
+        const re = /\(([a-z.]+),(\(\)),(\(\))\)/i;
+        console.log(re.exec(serialized));
+        return '';
+        while (re.exec(serialized) !== null) {
+            serialized = serialized.replace(re, '');
+            console.log(serialized);
+        }
+        return serialized;
+    }
 }
-const INPUT = new CustomNode('root', new CustomNode('left', new CustomNode('left.left')), new CustomNode('right'));
-//assert deserialize(serialize(node)).left.left.val == 'left.left'
-if (INPUT.left)
-    if (INPUT.left.left)
-        console.log(INPUT.left.left.val);
-// const MUST_RETURN = [120, 60, 40, 30, 24];
-// class Solution {
-//     private reduce: number
-//     constructor(public nums: number[]) {
-//         this.reduce = nums.reduce((prev, current) => current * prev, 1)
-//     }
-//     check() { return this.nums.map((x: number): number => this.reduce * (x ** -1)) }
-// }
-// const solution = new Solution(INPUT);
-// console.log("Expected: ");
-// console.log(MUST_RETURN);
-// console.log("Solution: ")
-// console.log(solution.check());
+const INPUT = new SolutionNode('root', new SolutionNode('left', new SolutionNode('left.left')), new SolutionNode('right'));
+const MUST_RETURN = 'left.left';
+console.log(JSON.stringify(INPUT));
+const serialized = INPUT.serialize(INPUT);
+const deserialized = INPUT.deserialize(serialized);
+console.log(deserialized);
+console.log('Expected: ' + MUST_RETURN);
+if (deserialized.left && deserialized.left.left) {
+    console.log('Deserialized: ' + deserialized.left.left.val);
+}
+else {
+    throw new Error('Wrong Solution');
+}
--- problem003/src/main.ts
+++ problem003/src/main.ts
@@ -7,21 +7,34 @@ class SolutionNode {
     constructor(public val: string,
         public left: SolutionNode | undefined = undefined,
         public right: SolutionNode | undefined = undefined) { }
-    serialize(node: SolutionNode): string {
-        return '';
+    serialize(node: SolutionNode | undefined): string {
+        if(node === undefined){
+            return '()';
+        }
+        return `(${node.val},${this.serialize(node.left)},${this.serialize(node.right)})`;
     }
-    deserialize(serialized: string): SolutionNode {
-        return this;
+    deserialize(serialized: string): any { // TODO: NOT RESOLVED
+        console.log(serialized)
+        const re = /\(([a-z.]+),(\(\)),(\(\))\)/i;
+        while(re.exec(serialized) !== null){
+            serialized = serialized.replace(re, '')
+            console.log(serialized)
+        }
+        return serialized;
     }
 }
 
 const INPUT = new SolutionNode('root', new SolutionNode('left', new SolutionNode('left.left')), new SolutionNode('right'));
 const MUST_RETURN = 'left.left';
-
+console.log(JSON.stringify(INPUT));
 const serialized: string = INPUT.serialize(INPUT);
 const deserialized: SolutionNode = INPUT.deserialize(serialized);
+console.log(deserialized);
 
 console.log('Expected: ' + MUST_RETURN);
-if (deserialized.left)
-    if (deserialized.left.left)
-        console.log('Deserialized: ' + deserialized.left.left.val);
+if (deserialized.left && deserialized.left.left) {
+    console.log('Deserialized: ' + deserialized.left.left.val);
+} else {
+    throw new Error('Wrong Solution');
+}
+
